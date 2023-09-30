ea6ac8967873b0bb445292dcc0b947941b1f1e72
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 11:07:16 -0400

    add environment.ts

---
+++ src/environments/dist/environment.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    apiBaseUrl: 'https://culinarysharesapi.azurewebsites.net/api/v1/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://culinarysharesapi.azurewebsites.net/api/v1/'
+};
