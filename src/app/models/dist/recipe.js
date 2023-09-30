20e98b18a54c61e147c1b22596ef889c7722e636
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 18:16:20 -0400

    add RecipePaginationImpl class

--- src/app/models/dist/recipe.js
+++ src/app/models/dist/recipe.js
@@ -1,2 +1,11 @@
 "use strict";
 exports.__esModule = true;
+exports.RecipePaginationImpl = void 0;
+var RecipePaginationImpl = /** @class */ (function () {
+    function RecipePaginationImpl() {
+        this.pageSize = 8;
+        this.currentPageNumber = 1;
+    }
+    return RecipePaginationImpl;
+}());
+exports.RecipePaginationImpl = RecipePaginationImpl;
