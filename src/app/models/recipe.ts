861d7c4f5472c41d645d73573694b5bef86a805d
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 18:15:52 -0400

    add class impl

--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -1,14 +1,23 @@
 export interface RecipePagination {
-    totalCount: number;
-    pageSize: number;
-    currentPageNumber: number;
-    totalPages: number;
-    data: Recipe[];
-  }
-  
-  export interface Recipe {
-    recipeId: number;
-    name: string;
-    description: string;
-    imageUrl: string;
-  }
+  totalCount: number;
+  pageSize: number;
+  currentPageNumber: number;
+  totalPages: number;
+  data: Recipe[];
+}
+
+export interface Recipe {
+  recipeId: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
+
+export class RecipePaginationImpl implements RecipePagination {
+  totalCount!: number;
+  pageSize: number = 8;
+  currentPageNumber: number = 1;
+  totalPages!: number;
+  data!: Recipe[];
+}
+
