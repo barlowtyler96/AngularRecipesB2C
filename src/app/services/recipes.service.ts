88213d0bb29e363a9acba961bc7c7b1d6450cee3
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 16:14:51 -0400

    add HttpClient and environment variables

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { RecipePagination } from '../models/recipe'
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  constructor() { }
+  public getRecipePagination(page: number, pageSize:number): Observable<RecipePagination> {
+    return this.http.get<RecipePagination>(`${environment.apiBaseUrl}/recipes/recent?page=${page}&pageSize=${pageSize}`)
+  }
+  constructor(private http: HttpClient) { }
 }
