a962538d2d0960596cc1e998f8e7ba60f00c7413
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 16:16:17 -0400

    inject RecipesService and import RecipePagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipePagination } from '../../models/recipe'
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recipePagination!: RecipePagination;
 
+  constructor(private recipesService: RecipesService) { }
+
+  ngOnInit() {
+    this.loadData(this.recipePagination.currentPageNumber);
+  }
+
+  loadData(page: number) {
+    this.recipesService.getRecipePagination(page, this.recipePagination.pageSize).subscribe((response) => {
+      this.recipePagination.data = response.data;
+      this.recipePagination.totalCount = response.totalCount;
+      this.recipePagination.totalPages = response.totalPages;
+    });
+  }
 }
