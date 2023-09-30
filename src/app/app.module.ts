384797a36e72ceae1985b1010a39f4851fce6859
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 12:23:46 -0400

    move to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowseComponent } from './pages/browse/browse.component';
 import { ShareComponent } from './pages/share/share.component';
