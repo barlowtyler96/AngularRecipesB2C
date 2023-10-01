f92cd3d8c0789d6f214d12637732f4bc1732cdea
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-30 20:58:50 -0400

    update pageNumber on page change

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipePagination } from '../../models/recipe'
+import { RecipePaginationImpl } from '../../models/recipe'
 import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
@@ -8,16 +8,24 @@ import { RecipesService } from 'src/app/services/recipes.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  recipePagination!: RecipePagination;
+  recipePagination: RecipePaginationImpl = {
+    totalCount: 0,
+    pageSize: 8,
+    currentPageNumber: 1,
+    totalPages: 0,
+    data: []
+  }
 
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
     this.loadData(this.recipePagination.currentPageNumber);
+    console.log(this.recipePagination.data)
   }
 
   loadData(page: number) {
     this.recipesService.getRecipePagination(page, this.recipePagination.pageSize).subscribe((response) => {
+      this.recipePagination.currentPageNumber = response.currentPageNumber;
       this.recipePagination.data = response.data;
       this.recipePagination.totalCount = response.totalCount;
       this.recipePagination.totalPages = response.totalPages;
