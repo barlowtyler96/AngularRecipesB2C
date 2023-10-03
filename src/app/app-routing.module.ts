1e428e4e2ba2588e5a35ba5af3bfb0393c33a728
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-02 22:51:17 -0400

    import components for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowseComponent } from './pages/browse/browse.component';
+import { ShareComponent } from './pages/share/share.component';
+import { YourSharesComponent } from './pages/your-shares/your-shares.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'browse', component: BrowseComponent},
+  {path: 'share', component: ShareComponent},
+  {path: 'yourShares', component: YourSharesComponent},
+  {path: 'favorites', component: FavoritesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dist/app-routing.module.js
@@ -0,0 +1,35 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppRoutingModule = void 0;
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var home_component_1 = require("./pages/home/home.component");
+var browse_component_1 = require("./pages/browse/browse.component");
+var share_component_1 = require("./pages/share/share.component");
+var your_shares_component_1 = require("./pages/your-shares/your-shares.component");
+var favorites_component_1 = require("./pages/favorites/favorites.component");
+var routes = [
+    { path: 'home', component: home_component_1.HomeComponent },
+    { path: 'browse', component: browse_component_1.BrowseComponent },
+    { path: 'share', component: share_component_1.ShareComponent },
+    { path: 'yourShares', component: your_shares_component_1.YourSharesComponent },
+    { path: 'favorites', component: favorites_component_1.FavoritesComponent }
+];
+var AppRoutingModule = /** @class */ (function () {
+    function AppRoutingModule() {
+    }
+    AppRoutingModule = __decorate([
+        core_1.NgModule({
+            imports: [router_1.RouterModule.forRoot(routes)],
+            exports: [router_1.RouterModule]
+        })
+    ], AppRoutingModule);
+    return AppRoutingModule;
+}());
+exports.AppRoutingModule = AppRoutingModule;
