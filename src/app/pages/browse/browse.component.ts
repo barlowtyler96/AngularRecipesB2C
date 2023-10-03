a197797f34a047f394bc36079ca5c7d056b30328
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-02 22:56:18 -0400

    add search and load data for paging

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RecipePagination } from 'src/app/models/recipe';
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-browse',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./browse.component.scss']
 })
 export class BrowseComponent {
+  searchText = '';
+  recipePagination: RecipePagination = {
+    currentPageNumber: 1,
+    pageSize: 8,
+    totalCount: 0,
+    totalPages: 0,
+    data: []
+  };
 
+  constructor(private recipesService: RecipesService) { }
+  
+  onSearch(searchText: string) { 
+    this.searchText = searchText
+    this.loadData(1);
+  }
+
+  loadData(page: number) {
+    this.recipesService.getRecipePaginationByKeyword(this.searchText, page, this.recipePagination.pageSize)
+      .subscribe((data) => {
+        this.recipePagination = data;
+      });
+  }
 }
