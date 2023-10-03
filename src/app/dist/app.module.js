7a40df738154bd74754be58bd6d31753bd7148f8
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-02 22:54:56 -0400

    module changes

--- src/app/dist/app.module.js
+++ src/app/dist/app.module.js
@@ -12,7 +12,6 @@ var platform_browser_1 = require("@angular/platform-browser");
 var http_1 = require("@angular/common/http");
 var app_routing_module_1 = require("./app-routing.module");
 var app_component_1 = require("./app.component");
-var layout_component_1 = require("./layout/layout.component");
 var sidebar_component_1 = require("./components/sidebar/sidebar.component");
 var navbar_component_1 = require("./components/navbar/navbar.component");
 var home_component_1 = require("./pages/home/home.component");
@@ -20,6 +19,10 @@ var browse_component_1 = require("./pages/browse/browse.component");
 var share_component_1 = require("./pages/share/share.component");
 var your_shares_component_1 = require("./pages/your-shares/your-shares.component");
 var favorites_component_1 = require("./pages/favorites/favorites.component");
+var pagination_component_1 = require("./components/pagination/pagination.component");
+var recipe_cards_component_1 = require("./components/recipe-cards/recipe-cards.component");
+var truncate_pipe_1 = require("./pipes/truncate.pipe");
+var forms_1 = require("@angular/forms");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
@@ -27,19 +30,22 @@ var AppModule = /** @class */ (function () {
         core_1.NgModule({
             declarations: [
                 app_component_1.AppComponent,
-                layout_component_1.LayoutComponent,
                 sidebar_component_1.SidebarComponent,
                 navbar_component_1.NavbarComponent,
                 home_component_1.HomeComponent,
                 browse_component_1.BrowseComponent,
                 share_component_1.ShareComponent,
                 your_shares_component_1.YourSharesComponent,
-                favorites_component_1.FavoritesComponent
+                favorites_component_1.FavoritesComponent,
+                pagination_component_1.PaginationComponent,
+                recipe_cards_component_1.RecipeCardsComponent,
+                truncate_pipe_1.TruncatePipe,
             ],
             imports: [
                 platform_browser_1.BrowserModule,
                 app_routing_module_1.AppRoutingModule,
-                http_1.HttpClientModule
+                http_1.HttpClientModule,
+                forms_1.FormsModule
             ],
             providers: [],
             bootstrap: [app_component_1.AppComponent]
