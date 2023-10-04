f43ac420d67faa00213508836dc70a349966e1aa
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-04 19:39:24 -0400

    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,24 @@ import { BrowseComponent } from './pages/browse/browse.component';
 import { ShareComponent } from './pages/share/share.component';
 import { YourSharesComponent } from './pages/your-shares/your-shares.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { MsalGuard } from '@azure/msal-angular';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent, data: { animation: 'isFirst' } },
   {path: 'browse', component: BrowseComponent, data: { animation: 'isSecond' } },
-  {path: 'share', component: ShareComponent, data: { animation: 'isThird' } },
-  {path: 'yourShares', component: YourSharesComponent},
-  {path: 'favorites', component: FavoritesComponent}
+  {path: 'share', component: ShareComponent, data: { animation: 'isThird' }, 
+  canActivate: [MsalGuard] },
+  {path: 'yourShares', component: YourSharesComponent, data: { animation: 'isFourth' }, 
+  canActivate: [MsalGuard]},
+  {path: 'favorites', component: FavoritesComponent, data: { animation: 'isFifth' }, 
+  canActivate: [MsalGuard]}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation:'enabledBlocking'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
