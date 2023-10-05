599c48642339c1d99c6f75b64ee3231352103c50
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-05 13:09:19 -0400

    navbar working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { MSAL_GUARD_CONFIG, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
+import { RedirectRequest } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @Input() loginDisplay = false;
+  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,  private authService: MsalService) { }
 
+  login() {
+    if (this.msalGuardConfig.authRequest){
+      this.authService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
+    } else {
+      this.authService.loginRedirect();
+    }
+  }
+
+  logout() { 
+    this.authService.logoutRedirect({
+      postLogoutRedirectUri: 'http://localhost:4200/'
+    });
+  }
 }
