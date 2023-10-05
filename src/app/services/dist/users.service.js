821407f3a34d7fbc10bb57ae051c164b6468e9b8
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-05 19:20:05 -0400

    add methods for usersService

---
+++ src/app/services/dist/users.service.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.UsersService = void 0;
+var environment_development_1 = require("src/environments/environment.development");
+var core_1 = require("@angular/core");
+var UsersService = /** @class */ (function () {
+    function UsersService(http) {
+        this.http = http;
+    }
+    UsersService.prototype.getUserFavoriteIds = function () {
+        return this.http.get(environment_development_1.environment.apiBaseUrl + "Users/favoritesIds");
+    };
+    UsersService.prototype.deleteUserFavorite = function (recipeId) {
+        var url = environment_development_1.environment.apiBaseUrl + "Users/favorite/" + recipeId;
+        return this.http["delete"](url);
+    };
+    UsersService.prototype.addUserFavorite = function (recipeId) {
+        var url = environment_development_1.environment.apiBaseUrl + "Users/favorite/" + recipeId;
+        return this.http.post(url, {});
+    };
+    UsersService = __decorate([
+        core_1.Injectable({
+            providedIn: 'root'
+        })
+    ], UsersService);
+    return UsersService;
+}());
+exports.UsersService = UsersService;
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getUserFavoriteIds(): Observable<number[]> {
+    return this.http.get<number[]>(`${environment.apiBaseUrl}Users/favoritesIds`)
+  } 
+
+  public deleteUserFavorite(recipeId: number): Observable<any> {
+    const url = `${environment.apiBaseUrl}Users/favorite/${recipeId}`;
+    return this.http.delete(url);
+  }
+
+  public addUserFavorite(recipeId: number): Observable<any> {
+    const url = `${environment.apiBaseUrl}Users/favorite/${recipeId}`;
+    return this.http.post(url, {});
+  }
 }
