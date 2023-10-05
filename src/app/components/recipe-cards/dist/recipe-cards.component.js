a44fc70d199e06866fe3dfe5da502358e86286ac
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-05 19:20:31 -0400

    add methods for favoriting and getting fav ids

--- src/app/components/recipe-cards/dist/recipe-cards.component.js
+++ src/app/components/recipe-cards/dist/recipe-cards.component.js
@@ -8,11 +8,57 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.RecipeCardsComponent = void 0;
 var core_1 = require("@angular/core");
+var msal_browser_1 = require("@azure/msal-browser");
+var rxjs_1 = require("rxjs");
 var RecipeCardsComponent = /** @class */ (function () {
-    function RecipeCardsComponent() {
+    function RecipeCardsComponent(usersService, authService, msalBroadcastService) {
+        this.usersService = usersService;
+        this.authService = authService;
+        this.msalBroadcastService = msalBroadcastService;
+        this.userFavoriteIds = [];
+        this.isSignedIn = false;
     }
+    RecipeCardsComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.msalBroadcastService.msalSubject$
+            .pipe(rxjs_1.filter(function (msg) { return msg.eventType === msal_browser_1.EventType.LOGIN_SUCCESS; }))
+            .subscribe(function (result) {
+            console.log(result);
+        });
+        this.msalBroadcastService.inProgress$
+            .pipe(rxjs_1.filter(function (status) { return status === msal_browser_1.InteractionStatus.None; }))
+            .subscribe(function () {
+            _this.setSignedInStatus();
+        });
+        if (this.isSignedIn) {
+            this.usersService.getUserFavoriteIds().subscribe({
+                next: function (favoriteIds) {
+                    _this.userFavoriteIds = favoriteIds;
+                },
+                error: function (error) {
+                    console.error('Error fetching user favorite IDs:', error);
+                }
+            });
+        }
+    };
+    RecipeCardsComponent.prototype.setSignedInStatus = function () {
+        this.isSignedIn = this.authService.instance.getAllAccounts().length > 0;
+    };
     RecipeCardsComponent.prototype.showFullRecipe = function (recipeId) {
         this.selectedRecipeId = recipeId;
+    };
+    RecipeCardsComponent.prototype.postUserFavorite = function (recipeId, isFavorite) {
+        if (isFavorite) {
+            this.usersService.deleteUserFavorite(recipeId).subscribe({});
+            var index = this.userFavoriteIds.indexOf(recipeId);
+            if (index !== -1) {
+                this.userFavoriteIds.splice(index, 1);
+            }
+        }
+        else if (!isFavorite) {
+            this.usersService.addUserFavorite(recipeId).subscribe({});
+            this.userFavoriteIds.push(recipeId);
+        }
     };
     __decorate([
         core_1.Input()
--- src/app/components/recipe-cards/recipe-cards.component.ts
+++ src/app/components/recipe-cards/recipe-cards.component.ts
@@ -1,17 +1,73 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+import { Observable, filter } from 'rxjs';
 import { Recipe } from 'src/app/models/recipe';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-recipe-cards',
   templateUrl: './recipe-cards.component.html',
   styleUrls: ['./recipe-cards.component.scss']
 })
-export class RecipeCardsComponent {
+export class RecipeCardsComponent implements OnInit {
   @Input() recipes!: Recipe[];
-  @Input() recipe!: Recipe; // Input property for recipe data
+  @Input() recipe!: Recipe;
+  userFavoriteIds: number[] = [];
   selectedRecipeId!: number;
-  
+  isSignedIn = false;
+
+  constructor(private usersService: UsersService, private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
+
+  ngOnInit(): void {
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+      )
+      .subscribe((result: EventMessage) => {
+        console.log(result);
+      });
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None)
+      )
+      .subscribe(() => {
+        this.setSignedInStatus();
+      })
+
+      if (this.isSignedIn) {
+        this.usersService.getUserFavoriteIds().subscribe({
+          next: (favoriteIds: number[]) => {
+            this.userFavoriteIds = favoriteIds;
+          },
+          error: (error: any) => {
+            console.error('Error fetching user favorite IDs:', error);
+          }
+        });
+      }
+  }
+
+  setSignedInStatus() {
+    this.isSignedIn = this.authService.instance.getAllAccounts().length > 0;
+  }
+
   showFullRecipe(recipeId: number) {
     this.selectedRecipeId = recipeId;
+  }
+
+  postUserFavorite(recipeId: number, isFavorite: boolean) {
+    if (isFavorite) {
+      this.usersService.deleteUserFavorite(recipeId).subscribe({
+      });
+      const index = this.userFavoriteIds.indexOf(recipeId);
+      if (index !== -1) {
+        this.userFavoriteIds.splice(index, 1);
+      }
+    } else if (!isFavorite) {
+      this.usersService.addUserFavorite(recipeId).subscribe({
+      });
+      this.userFavoriteIds.push(recipeId);
+    }
   }
 }
