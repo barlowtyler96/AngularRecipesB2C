44ca41232daea81222c77b340c102dd56ab861be
Tyler Barlow <barlowtyler96@gmail.com>
2023-10-11 20:52:07 -0400

    add recipe cards to display user's shares

--- src/app/pages/your-shares/your-shares.component.ts
+++ src/app/pages/your-shares/your-shares.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe } from 'src/app/models/recipe';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-your-shares',
   templateUrl: './your-shares.component.html',
   styleUrls: ['./your-shares.component.scss']
 })
-export class YourSharesComponent {
+export class YourSharesComponent implements OnInit {
+  recipes$!: Observable<Recipe[]>;
 
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.recipes$ = this.usersService.getUserCreatedRecipes();
+  }
 }
